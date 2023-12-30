5819d51e153bf86451a9438f9f0daac3fb6cb7af
Sushil Prasad <cse.sushil@gmail.com>
2023-12-30 22:32:03 +0530

    basic security added

---
+++ springcloud/src/main/java/com/cloudcode/springcloud/config/SpringSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.cloudcode.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.csrf(csrf -> csrf.disable());
+        return httpSecurity.build();
+    }
+
+}
