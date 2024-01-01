d02dd21ae44a35e3a0594ebaeb16da6d338c5248
Sushil Prasad <cse.sushil@gmail.com>
2024-01-02 00:18:10 +0530

    added junit for health endpoint

--- springcloud/src/main/java/com/cloudcode/springcloud/config/SpringSecurityConfiguration.java
+++ springcloud/src/main/java/com/cloudcode/springcloud/config/SpringSecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
