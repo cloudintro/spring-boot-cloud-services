f5d9e54d180662c107e7369f370b312d5f7f3ebc
Sushil Prasad <cse.sushil@gmail.com>
2024-01-08 00:02:31 +0530

    added api gateway

--- api-gateway/src/main/java/com/cloudcode/springcloud/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/cloudcode/springcloud/ApiGatewayApplication.java
@@ -2,14 +2,14 @@ package com.cloudcode.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
-public class NamingServerApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NamingServerApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
